54ba8bd6e67ffcb9dca9ae834536389b3267510b
WonHyeongKim <dnjsgud0828@gmail.com>
2024-05-21 14:15:15 +0900

    add Default web site

--- js/payment.js
+++ js/payment.js
@@ -1,5 +1,17 @@
 class payment{
-    constructor(){
+    constructor(totalBill, discountBill){
+        this.totalBill = totalBill; //할인전 총 금액
+        this.discountBill = discountBill; //할인 금액
+        this.bill = bill; //결제해야할 금액
+    }
+}
+
+class handlePayment{
+    getCoupons(){
 
     }
+    getCreditCard(){
+
+    }
+    getCash(){}
 }
